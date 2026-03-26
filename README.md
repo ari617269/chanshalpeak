9d7fc5b55d6b734cce28fbe1fd1cf182ac59e772
Ari617269 <ari617269@protonmail.com>
2026-03-26 14:25:20 +0530

    [006_token_char_classif] Tokenizer character classification

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### Steps
 
+- [X] 006 Token char classification
 - [X] 005 File read memory
 - [X] 004 File size
 - [X] 003 Read file
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 int main(int argc, char* argv[]){
  if(argc<2){
   printf("./main ./test.js");
@@ -32,9 +33,31 @@ int main(int argc, char* argv[]){
   printf("fread fail");
   return 6;
  }
- printf("%ld",rc);
  printf("%s",fs);
- free(fs);
  fclose(f);
+ const int toklen=51;
+ char* tok=malloc(toklen*sizeof(char));
+ if(tok==NULL){
+  printf("tok malloc fail");
+  return 7;
+ }
+ long pos=0;
+ bool ts=false;
+ while(pos<size+1){
+  bool isNum = fs[pos]>=48&&fs[pos]<=57;
+  bool isAlC = fs[pos]>=65&&fs[pos]<=90;
+  bool isAlS = fs[pos]>=97&&fs[pos]<=122;
+  bool isAlp = isAlC||isAlS;
+  bool isAlNu = isAlp||isNum;
+  bool isWs = fs[pos]==32||fs[pos]==9||fs[pos]==10;
+  bool isQuo = fs[pos]==34;
+  bool isPnc = fs[pos]==46||fs[pos]==40||fs[pos]==41||fs[pos]==59||isQuo;
+  if(isAlNu)printf("A");
+  if(isWs)printf("W");
+  if(isPnc)printf("P");
+  pos++;
+ }
+ free(fs);
+ free(tok);
  return 0;
 }
